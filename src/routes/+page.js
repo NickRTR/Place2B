381fa2aebb493029546e61b3699e623605e8630b
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-29 23:54:46 +0100

    Error handling

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,21 +1,27 @@
 import pb from "$lib/pocketbase.js";
 
 export async function load() {
-	const buildings = await pb.collection("buildings").getFullList(200 /* batch size */, {
-		sort: "-created"
-	});
+	try {
+		const buildings = await pb.collection("buildings").getFullList(200 /* batch size */, {
+			sort: "-created"
+		});
 
-	const rooms = await pb.collection("rooms").getFullList(200 /* batch size */, {
-		sort: "-created"
-	});
+		const rooms = await pb.collection("rooms").getFullList(200 /* batch size */, {
+			sort: "-created"
+		});
 
-	const positions = await pb.collection("positions").getFullList(200 /* batch size */, {
-		sort: "-created"
-	});
+		const positions = await pb.collection("positions").getFullList(200 /* batch size */, {
+			sort: "-created"
+		});
 
-	return {
-		buildings,
-		rooms,
-		positions
-	};
+		return {
+			buildings,
+			rooms,
+			positions
+		};
+	} catch (e) {
+		return {
+			error: e
+		};
+	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,30 +5,40 @@
 <body>
 	<h1>Place2B</h1>
 
-	<div class="buildings">
-		<p>
-			<strong>Buildings:</strong>
-			{#each data.buildings as building}
-				<span>{building.title}, </span>
-			{/each}
-		</p>
-	</div>
+	{#if data.error}
+		<p class="error">Error: {data.error.message}</p>
+	{:else}
+		<div class="buildings">
+			<p>
+				<strong>Buildings:</strong>
+				{#each data.buildings as building}
+					<span>{building.title}, </span>
+				{/each}
+			</p>
+		</div>
 
-	<div class="rooms">
-		<p>
-			<strong>Rooms:</strong>
-			{#each data.rooms as room}
-				<span>{room.title}, </span>
-			{/each}
-		</p>
-	</div>
+		<div class="rooms">
+			<p>
+				<strong>Rooms:</strong>
+				{#each data.rooms as room}
+					<span>{room.title}, </span>
+				{/each}
+			</p>
+		</div>
 
-	<div class="positions">
-		<p>
-			<strong>Positions:</strong>
-			{#each data.positions as position}
-				<span>{position.title}, </span>
-			{/each}
-		</p>
-	</div>
+		<div class="positions">
+			<p>
+				<strong>Positions:</strong>
+				{#each data.positions as position}
+					<span>{position.title}, </span>
+				{/each}
+			</p>
+		</div>
+	{/if}
 </body>
+
+<style>
+	.error {
+		color: red;
+	}
+</style>
