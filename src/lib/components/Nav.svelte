a6def7dbdc9e8cf83587978796bf413651ca6fa5
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-12-30 00:54:25 +0100

    Add navigation | MVP is ready eee🎉

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,15 @@
+<nav>
+	<a href="/" title="Home" data-sveltekit-preload-code data-sveltekit-preload-data>Home</a>
+	<a
+		href="/add"
+		title="Add buildings, rooms, positions and items"
+		data-sveltekit-preload-code
+		data-sveltekit-preload-data>Add</a
+	>
+</nav>
+
+<style>
+	nav {
+		padding-block: 1rem;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<body>
+	<Nav />
+
+	<main>
+		<slot />
+	</main>
+</body>
+
+<style>
+	:global(body) {
+		margin: 0;
+		font-family: monospace;
+		font-size: 16px;
+	}
+
+	body {
+		max-width: 800px;
+		margin-inline: auto;
+	}
+</style>
